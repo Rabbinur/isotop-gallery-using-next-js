dbc58aa186c60ed18f353225a3e55ba614418d93
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-10-31 12:39:59 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+export const portfolioData = [
+  {
+    id: 1,
+    image: img1,
+  },
+  {
+    id: 2,
+    image: img2,
+  },
+  {
+    id: 3,
+    image: img3,
+  },
+  {
+    id: 4,
+    image: img4,
+  },
+  {
+    id: 5,
+    image: img5,
+  },
+  {
+    id: 6,
+    image: img6,
+  },
+  {
+    id: 7,
+    image: img7,
+  },
+  {
+    id: 8,
+    image: img8,
+  },
+  {
+    id: 9,
+    image: img9,
+  },
+];
